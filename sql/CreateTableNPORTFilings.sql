b0366fefe7eefe90186fb7dd63d1ea52ec1d2cdb
hasherr <eklein217@gmail.com>
2025-03-30 16:56:57 -0400

    Tweaking fund holdings table

--- sql/CreateTableNPORTFilings.sql
+++ sql/CreateTableNPORTFilings.sql
@@ -1,8 +1,10 @@
 CREATE TABLE NPORTFilings(
     FilingId CHAR(50) PRIMARY KEY NOT NULL,
-    EntityCIK CHAR(200) FOREIGN KEY REFERENCES SECEntities(CIK),  
-    SeriesClassCompositeKey CHAR(50) FOREIGN KEY REFERENCES SeriesClasses(SeriesClassCompositeKey),
+    EntityCIK CHAR(200),
+    SeriesClassCompositeKey CHAR(50),
     ReportingPeriodEnd DATE NOT NULL,
     TotalAssetsValue FLOAT,
-    TotalLiabilitiesValue FLOAT
+    TotalLiabilitiesValue FLOAT,
+    FOREIGN KEY (EntityCIK) REFERENCES SECEntities(CIK),
+    FOREIGN KEY (SeriesClassCompositeKey) REFERENCES SeriesClasses(SeriesClassCompositeKey)
 );
