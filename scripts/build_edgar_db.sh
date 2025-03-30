4aa638828e4e3b1cc98050f8e15ecb2eecc88ab8
hasherr <eklein217@gmail.com>
2025-03-29 21:13:02 -0400

    Build scripts

---
+++ scripts/build_edgar_db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+edgar_db = $WATCHDOG_EDGAR_DB_DIR
+echo "Building database at location $edgar_db"
+
+rm $edgar_db_var
+sqlite3 $edgar_db
+sqlite3 $edgar_db < ../sql/CreateTablesecEntities
