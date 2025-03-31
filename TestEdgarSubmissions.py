4bbf1f6040a96d191e6da2100467dac17b2a31d6
hasherr <eklein217@gmail.com>
2025-03-30 23:17:40 -0400

    try a different cik

--- TestEdgarSubmissions.py
+++ TestEdgarSubmissions.py
@@ -7,10 +7,18 @@ HEADERS = {
     'Referer': 'https://www.sec.gov'
 }
 
+# ('0000906352'),
+# ('0000886982'),
+# ('0001557156'),
+# ('0000102909'),
+# ('0001920453'),
+# ('0001331875'),
+# ('0000036405')
+
 for i in range(1, 41):
     num_raw = str(i)
     num = num_raw.rjust(3, '0')
-    url = f'https://data.sec.gov/submissions/CIK0000886982-submissions-{num}.json'
+    url = f'https://data.sec.gov/submissions/CIK0000906352-submissions-{num}.json'
     r = requests.get(url, headers = HEADERS)
-    if r.text.find('0000950123-08-011525') != -1:
+    if r.text.find('NPORT-P') != -1:
         print(i)
