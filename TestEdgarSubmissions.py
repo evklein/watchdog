28f2574b99d7bedd108666a1eb71770567ad522c
hasherr <eklein217@gmail.com>
2025-03-30 23:15:24 -0400

    Rjust

--- TestEdgarSubmissions.py
+++ TestEdgarSubmissions.py
@@ -9,7 +9,7 @@ HEADERS = {
 
 for i in range(1, 41):
     num_raw = str(i)
-    num = num_raw.ljust(3, '0')
+    num = num_raw.rjust(3, '0')
     url = f'https://data.sec.gov/submissions/CIK0000886982-submissions-{num}.json'
     r = requests.get(url, headers = HEADERS)
     print(r.text)
