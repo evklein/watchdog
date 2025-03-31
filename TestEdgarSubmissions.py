3b7a1870a853df82e7b2927dee7cb9b8b9e2d1e3
hasherr <eklein217@gmail.com>
2025-03-30 23:14:50 -0400

    Every filing

--- TestEdgarSubmissions.py
+++ TestEdgarSubmissions.py
@@ -10,7 +10,8 @@ HEADERS = {
 for i in range(1, 41):
     num_raw = str(i)
     num = num_raw.ljust(3, '0')
-    r = requests.get(f'https://data.sec.gov/submissions/CIK0000886982-submissions-{num}.json', headers = HEADERS)
+    url = f'https://data.sec.gov/submissions/CIK0000886982-submissions-{num}.json'
+    r = requests.get(url, headers = HEADERS)
     print(r.text)
     if r.text.find('NPORT-P') != -1:
         print(i)
