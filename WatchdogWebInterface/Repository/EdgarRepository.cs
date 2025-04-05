a2531be424c97e0de0f592ee88bb4142d45252a7
hasherr <eklein217@gmail.com>
2025-04-05 10:18:18 -0400

    Try fault tolerant sql reading

--- WatchdogWebInterface/Repository/EdgarRepository.cs
+++ WatchdogWebInterface/Repository/EdgarRepository.cs
@@ -10,9 +10,9 @@ public class EdgarRepository : IEdgarRepository
     public List<SECEntity> LoadSECEntities()
     {
         Console.WriteLine("Loading SEC ents");
-        string query = "SELECT * FROM SECEntities;";
+        string query = "SELECT * FROM SECEntities";
         List<SECEntity> entities = new();
-        using (var connection = new SqliteConnection("Data Source=../../../data/EdgarData.db"))
+        using (var connection = new SqliteConnection("Data Source=../../data/EdgarData.db"))
         {
             connection.Open();
             using (var command = new SqliteCommand(query, connection))
@@ -20,15 +20,16 @@ public class EdgarRepository : IEdgarRepository
                 using (var reader = command.ExecuteReader())
                 {
                     Console.WriteLine("INside reader");
-                    SECEntity entity = new()
-                    {
-                        CIK = reader.GetString(reader.GetOrdinal("CIK")),
-                        Name = reader.GetString(reader.GetOrdinal("Name")),
-                        EntityType = reader.GetString(reader.GetOrdinal("EntityType")),
-                        IncorporationState = reader.GetString(reader.GetOrdinal("IncorporationState")),
-                        PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber")),
-                        Address = reader.GetString(reader.GetOrdinal("Address"))
-                    };
+                    SECEntity entity = new();
+
+                    // Safe way to get string values that might be NULL
+                    entity.CIK = reader.IsDBNull(reader.GetOrdinal("CIK")) ? null : reader.GetString(reader.GetOrdinal("CIK"));
+                    entity.Name = reader.IsDBNull(reader.GetOrdinal("Name")) ? null : reader.GetString(reader.GetOrdinal("Name"));
+                    entity.EntityType = reader.IsDBNull(reader.GetOrdinal("EntityType")) ? null : reader.GetString(reader.GetOrdinal("EntityType"));
+                    entity.IncorporationState = reader.IsDBNull(reader.GetOrdinal("IncorporationState")) ? null : reader.GetString(reader.GetOrdinal("IncorporationState"));
+                    entity.PhoneNumber = reader.IsDBNull(reader.GetOrdinal("PhoneNumber")) ? null : reader.GetString(reader.GetOrdinal("PhoneNumber"));
+                    entity.Address = reader.IsDBNull(reader.GetOrdinal("Address")) ? null : reader.GetString(reader.GetOrdinal("Address"));
+
 
                     // Only add if it has valid filings.
                     // string countQuery = $"SELECT COUNT(*) FROM NPORTFilings WHERE EntityCIK = '{entity.CIK}'";
