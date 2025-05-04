470b8fb841eb1f5d1ec0aab84a14839bae31a2f8
hasherr <eklein217@gmail.com>
2025-05-04 15:36:18 -0400

    Try static files again

--- WatchdogWebInterface/Program.cs
+++ WatchdogWebInterface/Program.cs
@@ -5,6 +5,7 @@ using WatchdogWebInterface.Areas.Identity.Data;
 using Radzen;
 using static Microsoft.AspNetCore.Http.StatusCodes;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using System.Security.Cryptography;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("WatchdogWebInterfaceIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'WatchdogWebInterfaceIdentityDbContextConnection' not found."); ;
@@ -43,6 +44,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseStaticFiles();
 }
 
 //app.UseHttpsRedirection();
@@ -51,7 +53,6 @@ app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.UseStaticFiles();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 app.MapRazorPages();
